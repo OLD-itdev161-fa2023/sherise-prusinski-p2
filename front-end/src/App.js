bf5b0b031fa3475af15f2dfe17723cdf62889313
Sherise Prusinski <98294721+prusinskis@users.noreply.github.com>
2022-12-12 07:04:30 -0600

    Added Router, Routes, Route in React app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import './App.css';
 import api from "./api"
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
@@ -30,7 +30,7 @@ class App extends React.Component {
     e.preventDefault()
     const newTask = await api.createTask(this.state.taskDesc)
     this.setState({
-      tasks:[...this.state.tasks, newTask]
+      tasks: [...this.state.tasks, newTask]
     })
   }
 
@@ -39,7 +39,7 @@ class App extends React.Component {
       e.stopPropagation()
       await api.deleteTask(id)
       this.setState({
-        tasks:[...this.state.tasks.filter(({ _id: i }) => id !== i)]
+        tasks: [...this.state.tasks.filter(({ _id: i }) => id !== i)]
       })
     } catch (err) { }
   }
@@ -51,40 +51,89 @@ class App extends React.Component {
     }
     const updatedTask = await api.updateTask(id, payload)
     this.setState({
-      tasks:[...this.state.tasks.map(task => (task._id === id ? updatedTask : task))]
+      tasks: [...this.state.tasks.map(task => (task._id === id ? updatedTask : task))]
     })
   }
 
-  render() {
-    return (
-      <div className="App" >
-        <header className="App-header">
-          TaskKeeper
-        </header>
-        <div>
-          <input
-            id="task-input"
-            type="text"
-            value={this.state.taskDesc}
-            onChange={({ target }) => this.setState({taskDesc:target.value})}
-          />
-          <button type="button" onClick={this.createTask}>
-            Add
-          </button>
-          <ul>
-            {this.state.tasks.map(({ _id, taskDescription, completed }, i) => (
-              <li key={i}
-                onClick={e => this.updateTask(e, _id)}
-                className={completed ? "completed" : "pending"}
-              >
-              {taskDescription}
-              <span onClick={e => this.deleteTask(e, _id)}>X</span>
-              </li>
-            ))}
-          </ul>
+  logOut = async () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.setState({ user: null, token: null });
+  }
 
+  render() {
+    const token = localStorage.getItem('token');
+    let { user, tasks, taskDesc } = this.state;
+    const authProps = {
+      authenticateUser: api.authenticateUser
+    };
+
+    return (
+      <Router>
+        <div className="App" >
+          <header className="App-header">
+            <h1>TaskKeeper</h1>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                {user ? (
+                  <Link to="/new-task">New Task</Link>
+                ) : (
+                  <Link to="/register">Register</Link>
+                )}
+              </li>
+              <li>
+                {user ? (
+                  <Link to="" onClick={this.logOut}>Log out</Link>
+                ) : (
+                  <Link to="/login">Login</Link>
+                )}
+              </li>
+            </ul>
+          </header>
+          <main>
+            <Routes>
+              <Route exact path="/" element= {
+                  <div>
+                    <input
+                      id="task-input"
+                      type="text"
+                      value={taskDesc}
+                      onChange={({ target }) => this.setState({ taskDesc: target.value })}
+                    />
+                    <button type="button" onClick={this.createTask}>
+                      Add
+                    </button>
+                    <ul>
+                      {tasks.map(({ _id, taskDescription, completed }, i) => (
+                        <li key={i}
+                          onClick={e => this.updateTask(e, _id)}
+                          className={completed ? "completed" : "pending"}
+                        >
+                          {taskDescription}
+                          <span onClick={e => this.deleteTask(e, _id)}>X</span>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                }>
+              </Route>
+              <Route
+                exact
+                path="/register"
+                render={() => <Register {...authProps} />}
+              />
+              <Route
+                exact
+                path="/login"
+                render={() => <Login {...authProps} />}
+              />
+            </Routes>
+          </main>
         </div>
-      </div>
+      </Router>
     );
   }
 }
