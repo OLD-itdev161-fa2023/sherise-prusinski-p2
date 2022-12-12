7edf86496561517aa9b4c847a99c356ad99365c5
Sherise Prusinski <98294721+prusinskis@users.noreply.github.com>
2022-12-11 20:14:27 -0600

    App converted to React component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,72 +7,84 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
 
-function App() {
-  const [tasks, setTasks] = useState([]);
-  const [taskDesc, setTask] = useState("");
-  let task;
+class App extends React.Component {
+  state = {
+    tasks: [],
+    task: null,
+    taskDesc: null
+  };
 
-  useEffect(() => {
+  componentDidMount() {
     const fetchTaskAndSetTasks = async () => {
-      const tasks = await api.getTasks()
-      setTasks(tasks)
+      const fetchedTasks = await api.getTasks()
+      this.setState({
+        tasks: fetchedTasks
+      })
     }
     fetchTaskAndSetTasks()
-  }, [])
-
-  const createTask = async e => {
-    e.preventDefault()
-    const newTask = await api.createTask(taskDesc)
-    setTasks([...tasks, newTask])
   }
 
-  const deleteTask = async (e, id) => {
+  createTask = async e => {
+    e.preventDefault()
+    const newTask = await api.createTask(this.state.taskDesc)
+    this.setState({
+      tasks:[...this.state.tasks, newTask]
+    })
+  }
+
+  deleteTask = async (e, id) => {
     try {
       e.stopPropagation()
       await api.deleteTask(id)
-      setTasks(tasks.filter(({ _id: i }) => id !== i))
+      this.setState({
+        tasks:[...this.state.tasks.filter(({ _id: i }) => id !== i)]
+      })
     } catch (err) { }
   }
 
-  const updateTask = async (e, id) => {
+  updateTask = async (e, id) => {
     e.stopPropagation()
     const payload = {
-      completed: !tasks.find(task => task._id === id).completed,
+      completed: !this.state.tasks.find(task => task._id === id).completed,
     }
     const updatedTask = await api.updateTask(id, payload)
-    setTasks(tasks.map(task => (task._id === id ? updatedTask : task)))
+    this.setState({
+      tasks:[...this.state.tasks.map(task => (task._id === id ? updatedTask : task))]
+    })
   }
 
-  return (
-    <div className="App" >
-      <header className="App-header">
-        TaskKeeper
-      </header>
-      <div>
-        <input
-          id="task-input"
-          type="text"
-          value={taskDesc}
-          onChange={({ target }) => setTask(target.value)}
-        />
-        <button type="button" onClick={createTask}>
-          Add
-        </button>
-        <ul>
-          {tasks.map(({ _id, taskDescription, completed }, i) => (
-            <li key={i}
-              onClick={e => updateTask(e, _id)}
-              className={completed ? "completed" : "pending"}
-            >
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          TaskKeeper
+        </header>
+        <div>
+          <input
+            id="task-input"
+            type="text"
+            value={this.state.taskDesc}
+            onChange={({ target }) => this.setState({task:target.value})}
+          />
+          <button type="button" onClick={this.createTask}>
+            Add
+          </button>
+          <ul>
+            {this.state.tasks.map(({ _id, taskDescription, completed }, i) => (
+              <li key={i}
+                onClick={e => this.updateTask(e, _id)}
+                className={completed ? "completed" : "pending"}
+              >
               {taskDescription}
-              {task} <span onClick={e => deleteTask(e, _id)}>X</span>
-            </li>
-          ))}
-        </ul>
+              <span onClick={e => this.deleteTask(e, _id)}>X</span>
+              </li>
+            ))}
+          </ul>
 
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
